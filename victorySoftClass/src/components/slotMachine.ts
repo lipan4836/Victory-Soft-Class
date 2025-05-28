bafd1cabd4a7b7de38c31872038f518efa39273f
Anton <lipan48@gmail.com>
2025-05-28 21:40:36 +0300

    feat: start implement reels

--- victorySoftClass/src/components/slotMachine.ts
+++ victorySoftClass/src/components/slotMachine.ts
@@ -1,5 +1,5 @@
 import { Application, Container, Sprite, Texture, Text, Graphics } from 'pixi.js';
-import { SYMBOL_NAMES, SymbolName } from '../assets/symbols';
+import { SYMBOL_NAMES, type SymbolName } from '../assets/symbols';
 
 export class SlotMachine extends Container {
   private reels: Reel[] = []
@@ -15,5 +15,65 @@ export class SlotMachine extends Container {
     this.app = app
     this.position.set(app.screen.width / 2, app.screen.height / 2)
     this.pivot.set(this.width / 2, this.height / 2)
+  }
+
+  private createReels() {
+    for (let i = 0; i < this.reelCount; i += 1) {
+      const reel = new Reel
+    }
+  }
+}
+
+class Reel extends Container {
+  private symbols: Sprite[] = []
+  private symbolSize: number
+  private visibleSymbols: number
+  private reelIndex: number
+  private isSpinning = false
+  private speenSpeed = 0
+  private spinTime = 0
+  private spinDuration = 0
+  private onStopCallback: () => void
+  private currentPosition = 0
+  private targetPosition = 0
+
+  constructor(
+    index: number,
+    symbolSize: number,
+    visibleSymbols: number,
+    reelCount: number,
+    onStop: () => void
+  ) {
+    super()
+    this.reelIndex = index
+    this.symbolSize = symbolSize
+    this.visibleSymbols = visibleSymbols
+    this.onStopCallback = onStop
+    this.createSymbols()
+  }
+
+  private createSymbols() {
+    const totalSymbols = this.visibleSymbols + 2
+
+    for (let i = 0; i < totalSymbols; i += 1) {
+      const symbol = this.createRandomSymbol()
+      symbol.y = i * this.symbolSize
+      this.symbols.push(symbol)
+      this.addChild(symbol)
+    }
+  }
+
+  private createRandomSymbol() {
+    const randomIndex = Math.floor(Math.random() * SYMBOL_NAMES.length)
+    const symbolLable = SYMBOL_NAMES[randomIndex]
+    const texture = Texture.from(`png/${symbolLable}.png`)
+    const sprite = new Sprite(texture)
+
+    sprite.width = this.symbolSize
+    sprite.height = this.symbolSize
+    sprite.anchor.set(0.5)
+    sprite.x = this.symbolSize / 2
+
+    return sprite
   }
 }
